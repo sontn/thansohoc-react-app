30f13efab86c77ad8fefd629a6c4a00d3ee75042
Son Tran <son.tranngoc@gmail.com>
2024-06-04 10:59:54 +0700

    Add footer nhagiaodich.com

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [result, setResult] = useState('');
-  const [error, setError] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [result, setResult] = useState("");
+  const [error, setError] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setError('');    
-    setResult(''); // Clear the result when a new submission is made
-    
+    setError("");
+    setResult(""); // Clear the result when a new submission is made
 
     try {
-      const response = await fetch('https://obxapg4gkme4lskjhynzfxq5mu0royfb.lambda-url.us-east-1.on.aws', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ phone_number: phoneNumber }),
-      });
+      const response = await fetch(
+        "https://obxapg4gkme4lskjhynzfxq5mu0royfb.lambda-url.us-east-1.on.aws",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ phone_number: phoneNumber }),
+        }
+      );
 
       if (!response.ok) {
-        throw new Error('Network response was not ok');
+        throw new Error("Network response was not ok");
       }
 
       const data = await response.json();
-      console.log('Data:', data);
+      console.log("Data:", data);
       setResult(data);
-      
     } catch (error) {
-      setError('Số điện thoại nhập không đúng, vui lòng nhập lại.');
-      console.error('Error:', error);
+      setError("Số điện thoại nhập không đúng, vui lòng nhập lại.");
+      console.error("Error:", error);
     }
   };
 
@@ -39,8 +40,8 @@ function App() {
     <div className="App">
       <h1>Thần số học dựa vào số điện thoại</h1>
       <form onSubmit={handleSubmit}>
-      Số điện thoại
-        <label>          
+        Số điện thoại
+        <label>
           <input
             type="tel"
             value={phoneNumber}
@@ -52,7 +53,7 @@ function App() {
       </form>
       <div id="result">
         <h2>Kết quả:</h2>
-        <p>{result || 'Chưa có kết quả'}</p>
+        <p>{result || "Chưa có kết quả"}</p>
       </div>
       {error && (
         <div>
@@ -60,6 +61,16 @@ function App() {
           <p>{error}</p>
         </div>
       )}
+      <footer>
+        &copy; {new Date().getFullYear()}{" "}
+        <a
+          href="https://nhagiaodich.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          nhagiaodich.com
+        </a>
+      </footer>
     </div>
   );
 }
